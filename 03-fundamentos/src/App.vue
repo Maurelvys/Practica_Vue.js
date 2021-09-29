e43d4b5469920449451a14d0e08390ca4e57c882
Maurelvys <maurelvyslemus@gmail.com>
2021-09-29 17:35:25 -0400

    Culminada seccion 5

---
+++ 03-fundamentos/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+
+  <!-- <Counter :start="100" /> -->
+  <indecision />
+</template>
+
+<script>
+//import Counter from "./components/Counter.vue";
+
+import indecision from "./components/indecision.vue";
+
+export default {
+  name: 'App',
+  components: {
+    //Counter
+    indecision,
+  },
+}
+
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 03-fundamentos/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './css/style.css'
+
+createApp(App).mount('#app')
